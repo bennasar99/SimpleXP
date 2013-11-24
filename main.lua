b15101318c5ee75dc2ac53da9a39d449c8d55382
tonibm19 <tonibm19@gmail.com>
2013-11-24 12:53:16 +0100

    Improved xp gaining when mining

--- main.lua
+++ main.lua
@@ -10,17 +10,20 @@ function Initialize(Plugin)
 end
 
 function OnPlayerBrokenBlock(Player, BlockX, BlockY, BlockZ, BlockFace, BlockType, BlockMeta)
-	if Player:GetGameMode() == 0 then
-		if (BlockType == 16) then
+	local HeldItem = Player:GetEquippedItem();
+	local HeldItemType = HeldItem.m_ItemType;
+	if (Player:GetGameMode() == 0) then
+		if (BlockType == E_BLOCK_COAL_ORE) and (HeldItemType == E_ITEM_WOODEN_PICKAXE) or (HeldItemType == E_ITEM_STONE_PICKAXE) or (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
 			Player:DeltaExperience(math.random(0,2))
-		end
-		if (BlockType == 56) or (BlockType == 129) then
+		elseif (BlockType == E_BLOCK_DIAMOND_ORE) or (BlockType == E_BLOCK_ESMERALD_ORE) and (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
 			Player:DeltaExperience(math.random(3,7))
-		end
-		if (BlockType == 73) or (BlockType == 74) then
+		elseif (BlockType == E_BLOCK_LAPIS_ORE) and (HeldItemType == E_ITEM_STONE_PICKAXE) or (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
 			Player:DeltaExperience(math.random(2,5))
-		end
-		if (BlockType == 52) then
+		elseif (BlockType == E_BLOCK_NETHER_QUARTZ_ORE) and (HeldItemType == E_ITEM_WOODEN_PICKAXE) or (HeldItemType == E_ITEM_STONE_PICKAXE) or (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
+			Player:DeltaExperience(math.random(2,5))
+		elseif (BlockType == E_BLOCK_REDSTONE_ORE) or (BlockType == E_BLOCK_REDSTONE_ORE_GLOWING) and (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
+			Player:DeltaExperience(math.random(1,5))
+		elseif (BlockType == E_BLOCK_MOSTER_SPAWNER) then
 			Player:DeltaExperience(math.random(15,43))
 		end
 	end
