8e78afd094a628c9b00ab62b6f396d99f87212d1
tonibm19 <tonibm19@gmail.com>
2013-11-25 18:00:13 +0100

    First stable version (tested and no bugs found)

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ function Initialize(Plugin)
 	PluginManager = cRoot:Get():GetPluginManager();
 	PluginManager:AddHook(cPluginManager.HOOK_PLAYER_BROKEN_BLOCK, OnPlayerBrokenBlock)
 	PluginManager:AddHook(cPluginManager.HOOK_PLAYER_RIGHT_CLICK, OnPlayerRightClick)
+	cPluginManager.AddHook( cPluginManager.HOOK_KILLING, OnKilling )
 
 	LOG("Initialized " .. Plugin:GetName() .. " v" .. Plugin:GetVersion())
 	return true;
@@ -14,16 +15,26 @@ function OnPlayerBrokenBlock(Player, BlockX, BlockY, BlockZ, BlockFace, BlockTyp
 	local HeldItem = Player:GetEquippedItem();
 	local HeldItemType = HeldItem.m_ItemType;
 	if (Player:GetGameMode() == 0) then
-		if (BlockType == E_BLOCK_COAL_ORE) and (HeldItemType == E_ITEM_WOODEN_PICKAXE) or (HeldItemType == E_ITEM_STONE_PICKAXE) or (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
-			Player:DeltaExperience(math.random(0,2))
-		elseif (BlockType == E_BLOCK_DIAMOND_ORE) or (BlockType == E_BLOCK_ESMERALD_ORE) and (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
-			Player:DeltaExperience(math.random(3,7))
-		elseif (BlockType == E_BLOCK_LAPIS_ORE) and (HeldItemType == E_ITEM_STONE_PICKAXE) or (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
-			Player:DeltaExperience(math.random(2,5))
-		elseif (BlockType == E_BLOCK_NETHER_QUARTZ_ORE) and (HeldItemType == E_ITEM_WOODEN_PICKAXE) or (HeldItemType == E_ITEM_STONE_PICKAXE) or (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
-			Player:DeltaExperience(math.random(2,5))
-		elseif (BlockType == E_BLOCK_REDSTONE_ORE) or (BlockType == E_BLOCK_REDSTONE_ORE_GLOWING) and (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
-			Player:DeltaExperience(math.random(1,5))
+		if (BlockType == E_BLOCK_COAL_ORE) then
+			if (HeldItemType == E_ITEM_WOODEN_PICKAXE) or (HeldItemType == E_ITEM_STONE_PICKAXE) or (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
+				Player:DeltaExperience(math.random(0,2))
+			end
+		elseif (BlockType == E_BLOCK_DIAMOND_ORE) or (BlockType == E_BLOCK_ESMERALD_ORE) then
+			if (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
+				Player:DeltaExperience(math.random(3,7))
+			end
+		elseif (BlockType == E_BLOCK_LAPIS_ORE) then
+			if (HeldItemType == E_ITEM_STONE_PICKAXE) or (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
+				Player:DeltaExperience(math.random(2,5))
+			end
+		elseif (BlockType == E_BLOCK_NETHER_QUARTZ_ORE) then
+			if (HeldItemType == E_ITEM_WOODEN_PICKAXE) or (HeldItemType == E_ITEM_STONE_PICKAXE) or (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
+				Player:DeltaExperience(math.random(2,5))
+			end
+		elseif (BlockType == E_BLOCK_REDSTONE_ORE) or (BlockType == E_BLOCK_REDSTONE_ORE_GLOWING) then
+			if (HeldItemType == E_ITEM_IRON_PICKAXE) or (HeldItemType == E_ITEM_DIAMOND_PICKAXE) then
+				Player:DeltaExperience(math.random(1,5))
+			end
 		elseif (BlockType == E_BLOCK_MOSTER_SPAWNER) then
 			Player:DeltaExperience(math.random(15,43))
 		end
